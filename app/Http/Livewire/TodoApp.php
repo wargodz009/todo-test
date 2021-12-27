4e7af12104e7434feeb172e608f9bcab6f06be5c
jay-ar <jayar_mundala@yahoo.com>
2021-12-28 00:32:26 +0800

    adding livewire

---
+++ app/Http/Livewire/TodoApp.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App\Http\Livewire;
+
+use Livewire\Component;
+
+class TodoApp extends Component
+{
+    public function render()
+    {
+        return view('livewire.todo-app');
+    }
+}
