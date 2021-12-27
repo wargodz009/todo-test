c587226c57c96460196d9c99b592712edc19a532
jay-ar <jayar_mundala@yahoo.com>
2021-12-28 02:16:17 +0800

    basic crud

--- app/Http/Livewire/TodoApp.php
+++ app/Http/Livewire/TodoApp.php
@@ -2,12 +2,55 @@
 
 namespace App\Http\Livewire;
 
+use App\Models\Todo;
 use Livewire\Component;
 
 class TodoApp extends Component
 {
+    public $newTodo = '';
+    public $updateTodoId = '';
+    public $oldTodoValue = '';
+
     public function render()
     {
-        return view('livewire.todo-app');
+        $allTodo = Todo::where('user_id',auth()->user()->id)->get();
+        return view('livewire.todo-app',[
+            'todos' => $allTodo
+        ]);
+    }
+    public function addTodo() {
+        $this->validate(['newTodo' => 'required',],['newTodo.required' => 'A new todo can\'t be empty.',]);
+        $todo = new Todo();
+        $todo->value = $this->newTodo;
+        $todo->user_id = auth()->user()->id;
+        $todo->save();
+        $this->newTodo = '';
+        session()->flash('message', 'Todo successfully Added.');
+    }
+    public function deleteTodo($todoid) {
+        Todo::destroy($todoid);
+        session()->flash('message', 'Todo successfully Deleted.');
+    }
+
+    public function updateTodoStatus($todoId,$todoStatus)
+    {
+        Todo::where('id',$todoId)->update(
+            ['status'=> !$todoStatus]
+        );
+        session()->flash('message', 'Todo successfully Updated.');
+    }
+    public function updateTodoValue($todoId)
+    {
+        Todo::where('id',$todoId)->update(
+            ['value'=> $this->oldTodoValue]
+        );
+        $this->updateTodoId = '';
+        $this->oldTodoValue = '';
+        session()->flash('message', 'Todo successfully Updated.');
+    }
+    public function setUpdateTodoValue($todoId,$oldValue)
+    {
+        $this->updateTodoId = $todoId;
+        $this->oldTodoValue = $oldValue;
     }
 }
--- resources/views/livewire/todo-app.blade.php
+++ resources/views/livewire/todo-app.blade.php
@@ -1,3 +1,32 @@
 <div>
+    <div>
+        @if (session()->has('message'))
+            <div class="alert alert-success">
+                {{ session('message') }}
+            </div>
+        @endif
+    </div>
+    <h1 class="text-6xl font-bold leading-tight text-center text-base">Todo List</h1>
 
+    <label for="new-todo"></label>
+    <input wire:model="newTodo"
+     wire:keydown.enter="addTodo()"
+     class="flex-grow block pt-4 pb-4 pl-2 text-2xl italic font-normal text-gray-700 border-l-2 border-indigo-600 outline-none"
+     type="text" name="new-todo" id="new-todo" placeholder="add Todo ?" autofocus>
+
+    <ul>
+    @foreach($todos as $todo)
+        @if($updateTodoId == $todo->id)
+                <label for=""></label>
+                <input type="text" wire:model="oldTodoValue">
+                <i wire:click="updateTodoValue({{ $todo->id }})">Save</i>
+        @else
+            <li class="@if($todo->status == 0) line-through @endif">{{ $todo->value }}
+                <i wire:click="deleteTodo({{ $todo->id }})">delete</i>
+                <i wire:click="updateTodoStatus({{ $todo->id }},{{ $todo->status }})">@if($todo->status == 1) Complete @else Incomplete @endif </i>
+                <i wire:click="setUpdateTodoValue({{ $todo->id }},'{{ $todo->value }}')">Update</i>
+            </li>
+        @endif
+    @endforeach
+    </ul>
 </div>
